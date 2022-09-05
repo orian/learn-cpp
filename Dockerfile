9d0c22e0e4d3eaa44005ea547441f5844bff9895
Paweł Szczur <pawelszczur+gh@gmail.com>
2022-09-05 06:41:30 +0200

    csv parser

--- Dockerfile
+++ Dockerfile
@@ -3,15 +3,17 @@ FROM ubuntu:20.04
 
 # Update apps on the base image
 RUN apt-get -y update && apt-get install -y
-RUN DEBIAN_FRONTEND=noninteractive TZ=Europe/Warsaw apt-get install -y build-essential cmake git wget apt-transport-https curl gnupg openjdk-11-jdk
+RUN DEBIAN_FRONTEND=noninteractive TZ=Europe/Warsaw apt-get install -y \
+    software-properties-common build-essential cmake git wget apt-transport-https curl gnupg openjdk-11-jdk
 RUN curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel-archive-keyring.gpg && \
     mv bazel-archive-keyring.gpg /usr/share/keyrings && \
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list && \
-    apt-get update &&  \
+    apt-get update -y && \
+    apt-get install -y bazel-5.3.0
 
 
 RUN add-apt-repository -y ppa:ubuntu-toolchain-r/test &&  \
-    apt install -y gcc-11 g++-11  \
+    apt-get install -y gcc-11 g++-11 && \
     update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 30 && \
     update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 30 && \
     update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-11 30 && \
--- csvparser/csvparser.cpp
+++ csvparser/csvparser.cpp
@@ -1,24 +1,96 @@
 #include <cstdio>
 #include <cassert>
+#include <cctype>
+#include <cstdint>
 
-#ACCEPT_SIGN(v) v =='-'
+class Parser {
+  size_t line = 0;
+  size_t line_value = 0;
+  FILE* file;
+  unsigned char c;
+  const char separator = ',';
+ public:
+  Parser(FILE* f) : file(f) {}
 
-#define PARSE_SIGNED_INT { \
-                           \
+  bool Next() {
+    c = fgetc(file);
+    return !feof(file);
+  }
+
+  int32_t ParseI32() {
+    int32_t num = 0;
+    bool neg = false;
+    if (c == '-') {
+      neg = true;
+      Next();
+    }
+    for (size_t i = 0; i < 10 && isdigit(c); ++i) {
+      num = 10 * num + (c - '0');
+      Next();
+    }
+    if (neg) num = -num;
+    return num;
+  }
+
+  uint32_t ParseU32() {
+    uint32_t num = 0;
+    for (size_t i = 0; i < 10 && isdigit(c); ++i) {
+      num = 10 * num + (c - '0');
+      Next();
+    }
+    return num;
+  }
+
+  void SkipWhitespaceSparatorWhitespace() {
+    while (isspace(c)) Next();
+    if (c == separator) {
+      ++line_value;
+      while (isspace(c)) Next();
+    } else if (c == '\n') {
+      ++line;
+      line_value = 0;
+    }
+  }
+};
+
+#define ACCEPT_SEPARATOR(v) {}
+
+#define ACCEPT_DIGIT(v) {}
+
+#define ACCEPT_N_DIGITS(n, v) {}
+
+#define ACCEPT_SIGN(v) { \
+  if ((v)=='-') next(); \
+}
+
+#define PARSE_SIGNED_INT(n) { \
+  ACCEPT_SIGN(v);             \
+  ACCEPT_N_DIGITS(3);         \
+};
+
+#define PARSE_INT8 {   \
+   PARSE_SIGNED_INT(8) \
 };
 
 int main() {
   auto fp = fopen("example.csv", "r");
   assert(fp != nullptr);
 
+  Parser parser(fp);
   char c;
-  while (1) {
+  size_t line = 0;
+  while (line == 0) {
     c = fgetc(fp);
     if (feof(fp)) {
       break;
+    } else if (c == '\n') {
+      ++line;
     }
     printf("%c", c);
   }
+  parser.ParseI32();
+  parser.SkipWhitespaceSparatorWhitespace();
+  parser.ParseU32();
 
   fclose(fp);
 }
